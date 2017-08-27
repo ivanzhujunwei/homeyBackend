9d4d156921f83023c9dffd78a3feedd2f20d9d01
tjhshiwo <tjhshiwo@gmail.com>
2017-08-27 10:00:13 +1000

    Fix the bug in class Connection, add connection() in getStatement(). fix the jason format in WriteToMySql.

--- homeyBack/src/com/model/ReturnAllHobby.java
+++ homeyBack/src/com/model/ReturnAllHobby.java
@@ -1,7 +1,5 @@
 package com.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,31 +10,11 @@ public class ReturnAllHobby {
 	static Statement st = null;
 	static ResultSet rs = null;
 
-	public static void connection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public String connectionToMySql() {
-		connection();
-		String host = "jdbc:mysql://localhost:3306/Homey?autoReconnect=true&useSSL=false";
-		String username = "root";
-		String password = "tjh199208";
-
-		// String host =
-		// "jdbc:mysql://au-cdbr-azure-southeast-a.cloudapp.net:3306/homeymsql?autoReconnect=true&useSSL=false";
-		// String username = "b84c8d66cf6a94";
-		// String password = "d3bbe525";
 
 		StringBuilder sb = new StringBuilder();
 		try {
 			rs = ConnectDB.getStatement().executeQuery("SELECT id,name,image FROM dict;");
-//			Connection connect = DriverManager.getConnection(host, username, password);
-//			st = connect.createStatement();
-//			rs = st.executeQuery("SELECT id,name,image FROM dict;");
 			sb.append("[");
 			while (rs.next()) {
 				sb.append("{\"id\":\"" + rs.getInt(1) + "\"," + "\"name\":\"" + rs.getString(2) + "\"," + "\"image\":\""
--- homeyBack/src/com/model/ReturnHobbyDetail.java
+++ homeyBack/src/com/model/ReturnHobbyDetail.java
@@ -10,33 +10,11 @@ public class ReturnHobbyDetail {
 	static Statement st = null;
 	static ResultSet rs = null;
 
-	public static void connection() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public String connectionToMySql(String input) {
-		connection();
-
-		String host = "jdbc:mysql://localhost:3306/Homey?autoReconnect=true&useSSL=false";
-		String username = "root";
-		String password = "tjh199208";
-		//
-		// String host =
-		// "jdbc:mysql://au-cdbr-azure-southeast-a.cloudapp.net:3306/homeymsql?autoReconnect=true&useSSL=false";
-		// String username = "b84c8d66cf6a94";
-		// String password = "d3bbe525";
-
 		StringBuilder sb = new StringBuilder();
 		try {
-//			Connection connect = DriverManager.getConnection(host, username, password);
-//			st = connect.createStatement();
-//			// rs = st.executeQuery("SELECT description,image,title FROM dict where id = "+  input +";");
-//			rs = st.executeQuery("SELECT description,image,title FROM dict where id = 1;");
-			rs = ConnectDB.getStatement().executeQuery("SELECT description,image,title FROM dict where id = "+  input +";");
+			rs = ConnectDB.getStatement()
+					.executeQuery("SELECT description,image,title FROM dict where id = " + input + ";");
 			while (rs.next()) {
 				sb.append("{\"description\":\"" + rs.getString(1) + "\"," + "\"image\":\"" + rs.getString(2) + "\","
 						+ "\"title\":\"" + rs.getString(3) + "\"}");
