412d3d43a6975521302d91d36540fa5b27224b77
tjhshiwo <tjhshiwo@gmail.com>
2017-09-19 14:56:47 +1000

    Add function addiction and financial

--- homeyBack/src/com/model/ReturnAddress.java
+++ homeyBack/src/com/model/ReturnAddress.java
@@ -69,37 +69,62 @@ public class ReturnAddress {
 		}
 	}
 
-	public static String returnAddFamVio(String postSub) {
-		if (postSub == null || postSub.length() == 0) {
-			return returnAdd("");
+	public static char[] returnAddFanacial(String subOrPost) {
+		// TODO Auto-generated method stub
+		// 写一个 servlet 叫issueservlet 然后 把url用if框起来，不同的if返回不同的id 然后调用同一个方法
+		return null;
+	}
+
+	public static String returnAddAddiction(String subOrPost) {
+		if (subOrPost == null || subOrPost.length() == 0) {
+			return returnAdd("", "12");
 		} else {
 			try {
-				Integer.valueOf(postSub);
-				String suburb = ReturnWeatherAndAct.getSuburb(postSub);
-				return returnAdd(suburb);
+				Integer.valueOf(subOrPost);
+				String suburb = ReturnWeatherAndAct.getSuburb(subOrPost);
+				return returnAdd(suburb, "12");
 			} catch (Exception e) {
 				if (e.toString().contains("string")) {
-					return returnAdd(postSub);
+					return returnAdd(subOrPost, "12");
 				}
 			}
 		}
 		return "no place";
 	}
 
-	public static String returnAdd(String suburb) {
+	public static String returnAddFamVio(String postSub) {
+		if (postSub == null || postSub.length() == 0) {
+			return returnAdd("", "11");
+		} else {
+			try {
+				Integer.valueOf(postSub);
+				String suburb = ReturnWeatherAndAct.getSuburb(postSub);
+				return returnAdd(suburb, "11");
+			} catch (Exception e) {
+				if (e.toString().contains("string")) {
+					return returnAdd(postSub, "11");
+				}
+			}
+		}
+		return "no place";
+	}
+
+	public static String returnAdd(String suburb, String id) {
 		StringBuilder sb = new StringBuilder();
 		try {
 			ConnectDB.closeConnection();
 			if (suburb.length() == 0) {
 				ConnectDB.rs = ConnectDB.getStatement().executeQuery(
-						"select name_of_trainer, address, suburb,comments, phone_number from hobby where kind = '11' order by comments;");
+						"select name_of_trainer, address, suburb,comments, phone_number from hobby where kind = '" + id
+								+ "' order by comments;");
 			} else {
 				ConnectDB.rs = ConnectDB.getStatement().executeQuery(
 						"select name_of_trainer, address, suburb,comments, phone_number from hobby where suburb ='"
-								+ suburb + "' and kind = '11' order by comments;");
+								+ suburb + "' and kind = '" + id + "' order by comments;");
 			}
 			sb.append("{\"allAddress\":\"");
 			while (ConnectDB.rs.next()) {
+				String abc = ConnectDB.rs.toString();
 				sb.append(ConnectDB.rs.getString(1));
 				sb.append("=");
 				sb.append(ConnectDB.rs.getString(2));
@@ -108,10 +133,13 @@ public class ReturnAddress {
 				sb.append("=");
 				sb.append(getIcon(ConnectDB.rs.getString(4).split("=")[0]));
 				sb.append("=");
-				sb.append(ConnectDB.rs.getString(5).replace(" ", "-"));
-				sb.append("=");
-				sb.append(ConnectDB.rs.getString(4).split("=")[returnDay()]);
-				if (ConnectDB.rs.getString(4).split("=").length == 9) {
+				sb.append(ConnectDB.rs.getString(5).replace(" ", ""));
+				if (ConnectDB.rs.getString(4).split("=").length == 1) {
+					sb.append("=");
+					sb.append(ConnectDB.rs.getString(4));
+				} else if (ConnectDB.rs.getString(4).split("=").length == 9) {
+					sb.append("=");
+					sb.append(ConnectDB.rs.getString(4).split("=")[returnDay()]);
 					sb.append("=");
 					sb.append(ConnectDB.rs.getString(4).split("=")[8]);
 				} else {
@@ -136,4 +164,5 @@ public class ReturnAddress {
 		calendar.setTime(now);
 		return calendar.get(Calendar.DAY_OF_WEEK);
 	}
+
 }
